8ae2634600469cd81b85e020db1daec55afd7cbb
Dale Ragan <dale.ragan@moncai.com>
2011-09-04 16:14:26 -0400

    [Unit Test Explorer] New integrated unit testing feature.
        * Note, old NUnit add-in has been removed from build process
          to minimize collisions with new add-in.

---
+++ main/src/addins/UnitTesting/MonoDevelop.UnitTesting.UnitTestExplorer/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+
+[assembly: AssemblyProduct("MonoDevelop")]
+[assembly: AssemblyTitle("Unit Test Explorer")]
+[assembly: AssemblyDescription("Allows you to visually explore and run your unit tests in the MonoDevelop IDE.  Support for testing frameworks are provided as separate add-ins.")]
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyCopyright("MIT X11")]
