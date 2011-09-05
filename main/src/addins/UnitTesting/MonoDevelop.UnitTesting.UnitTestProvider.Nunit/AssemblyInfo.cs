a61beb8fec6768d6793151351034c9a0bcf63402
Dale Ragan <dale.ragan@moncai.com>
2011-09-04 23:06:03 -0400

    [Unit Test Explorer] Initial NUnit Unit Test Provider.
        * Including the NUnit Unit Test Provider by default, since MonoDevelop
          already shipped with it.
        * When new NUnit versions come out in between MonoDevelop releases,
          we can update the add-in and have it available on the central
          add-in repository at addins.monodevelop.com.

---
+++ main/src/addins/UnitTesting/MonoDevelop.UnitTesting.UnitTestProvider.Nunit/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+
+[assembly: AssemblyProduct("MonoDevelop")]
+[assembly: AssemblyTitle("NUnit Test Provider")]
+[assembly: AssemblyDescription("Allows you to visually explore and run your unit tests developed with the NUnit Test Framework in the MonoDevelop IDE.")]
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyCopyright("MIT X11")]
